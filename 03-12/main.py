a749990bd3978e0f2771f2a75379079e86872ffa
Cyberkangaroo <j.kloos-1@web.de>
2023-12-03 16:34:02 +0100

    Day 3 part 2

--- 03-12/main.py
+++ 03-12/main.py
@@ -98,11 +98,89 @@ def check_adjacent(numbers, symbols):
                 neighbors.append([numbers[index][0] - 1, i])
             for i in range(numbers[index][1] - 1, numbers[index][2] + 2):
                 neighbors.append([numbers[index][0] + 1, i])
-            print(index, neighbors)
         for neighbor in neighbors:
             if neighbor in symbols:
                 parts.append(int(numbers[index][3]))
     return parts
+
+
+def find_gears():
+    lines = input_file
+    possible_gears = []
+    for line in lines:
+        line_list = list(line.strip())
+        for index in range(len(line_list)):
+            char = line_list[index]
+            if char == '*':
+                possible_gears.append([lines.index(line), index])
+    print(possible_gears)
+    gears = []
+    for candidate in possible_gears:
+        neighbors = []
+        number_count = 0
+        if candidate[0] == 0:                               #links oben
+            if candidate[1] == 0:
+                neighbors.append([0, 1])
+                neighbors.append([1, 0])
+                neighbors.append([1, 1])
+            elif candidate[1] == len(input_file[0])-1:      #rechts oben
+                neighbors.append([0, candidate[1]-1])
+                neighbors.append([1, candidate[1]])
+                neighbors.append([1, candidate[1] - 1])
+            else:                                           #obere kante
+                neighbors.append([0, candidate[1] - 1])
+                neighbors.append([0, candidate[1]+1])
+                neighbors.append([1, candidate[1]])
+                neighbors.append([1, candidate[1] - 1])
+                neighbors.append([1, candidate[1] + 1])
+        elif candidate[0] == len(input_file)-1:
+            if candidate[1] == 0:                           #links unten
+                neighbors.append([candidate[0], 1])
+                neighbors.append([candidate[0]-1, 0])
+                neighbors.append([candidate[0] - 1, 1])
+            elif candidate[1] == len(input_file[0])-1:      #rechts unten
+                neighbors.append([candidate[0], candidate[1]-1])
+                neighbors.append([candidate[0]-1, candidate[1]])
+                neighbors.append([candidate[0] - 1, candidate[1] - 1])
+            else:                                           #untere kante
+                neighbors.append([candidate[0], candidate[1] - 1])
+                neighbors.append([candidate[0], candidate[1] + 1])
+                neighbors.append([candidate[0]-1, candidate[1]])
+                neighbors.append([candidate[0] - 1, candidate[1] - 1])
+                neighbors.append([candidate[0] - 1, candidate[1] + 1])
+        elif candidate[1] == 0:                             #linke kante
+            neighbors.append([candidate[0], candidate[1]+1])
+            neighbors.append([candidate[0]-1, candidate[1]])
+            neighbors.append([candidate[0]+1, candidate[1]])
+            neighbors.append([candidate[0] - 1, candidate[1] + 1])
+            neighbors.append([candidate[0] + 1, candidate[1] + 1])
+        elif candidate[1] == len(input_file[0])-1:          #rechte kante
+            neighbors.append([candidate[0], candidate[1] - 1])
+            neighbors.append([candidate[0] - 1, candidate[1]])
+            neighbors.append([candidate[0] + 1, candidate[1]])
+            neighbors.append([candidate[0] - 1, candidate[1] - 1])
+            neighbors.append([candidate[0] + 1, candidate[1] - 1])
+        else:                                               #mitte
+            neighbors.append([candidate[0], candidate[1] - 1])
+            neighbors.append([candidate[0], candidate[1] + 1])
+            neighbors.append([candidate[0] - 1, candidate[1]])
+            neighbors.append([candidate[0] + 1, candidate[1]])
+            neighbors.append([candidate[0] - 1, candidate[1] - 1])
+            neighbors.append([candidate[0] + 1, candidate[1] - 1])
+            neighbors.append([candidate[0] - 1, candidate[1] + 1])
+            neighbors.append([candidate[0] + 1, candidate[1] + 1])
+        numbers = get_numbers()
+        numbers_pos = []
+        for number in numbers:
+            for index in range(number[1], number[2] + 1):
+                if [number[0], index] in neighbors:
+                    if number[3] not in numbers_pos:
+                        number_count += 1
+                        numbers_pos.append(number[3])
+        if number_count == 2:
+            gears.append([candidate, numbers_pos])
+    return gears
+
 
 def part1(numbers):
     sum = 0
@@ -111,4 +189,13 @@ def part1(numbers):
     return sum
 
 
-print(part1(check_adjacent(get_numbers(), get_symboles())))
+def part2():
+    sum = 0
+    gears = find_gears()
+    for gear in gears:
+        sum += int(gear[1][0]) * int(gear[1][1])
+    return sum
+
+
+print("Part 1: ", part1(check_adjacent(get_numbers(), get_symboles())))
+print("Part 2: ", part2())
